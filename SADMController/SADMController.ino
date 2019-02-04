b2c2404e11c14e666bd149329813f7dcccdee1dc
Thomas Mundal <thmundal@gmail.com>
2019-02-04 13:17:40 +0100

    Added some code to the SADM controller

--- SADMController/SADMController.ino
+++ SADMController/SADMController.ino
@@ -4,12 +4,38 @@
  Author:	thmun
 */
 
-// the setup function runs once when you press reset or power the board
-void setup() {
+#include <Stepper.h>
 
+const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
+// for your motor
+
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+uint8_t* serialBuffer;
+
+void setup() {
+	// set the speed at 60 rpm:
+	//myStepper.setSpeed(60);
+	// initialize the serial port:
+	Serial.begin(9600);
 }
 
-// the loop function runs over and over again until power down or reset
 void loop() {
-  
+	if (Serial.readBytesUntil('\n', serialBuffer, 8) > 0) {
+		Serial.println(*serialBuffer);
+	}
+	else {
+		Serial.println("No serial input received");
+	}
+
+
+	// step one revolution  in one direction:
+	//Serial.println("clockwise");
+	//myStepper.step(stepsPerRevolution);
+	//delay(500);
+
+	//// step one revolution in the other direction:
+	//Serial.println("counterclockwise");
+	//myStepper.step(-stepsPerRevolution);
+	//delay(500);
 }
