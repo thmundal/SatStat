c4a1540dd72f74d4984ae228a5e3d69812791a0b
Thomas Mundal <thmundal@gmail.com>
2019-02-13 22:35:37 +0100

    Unit tests for input interface

--- SatStat_SWLayer_Tests/UnitTest1.cs
+++ SatStat_SWLayer_Tests/UnitTest1.cs
@@ -6,29 +6,112 @@ using System.Collections.Generic;
 namespace SatStatTests
 {
     [TestClass]
-    public class UnitTest1 : DataReceiver<string>
+    public class UnitTest1
     {
-        string JSON = "{\"test\":\"value\"}";
-        string value = "value";
-        string received_payload;
-
-        public void ReceivePayload(string payload)
+        string JSON = "{\"double\":\"2,0\", \"string\":\"this is a string\", \"float\":\"3,2\", \"int\":\"5\", \"long\":\"5\", \"genericlist\":[\"one\",\"two\",\"three\"]}";
+        
+        [TestMethod]
+        public void ReceiveDouble()
         {
-            received_payload = payload;
+            DataStream stream = new DataStream();
+            Receiver<double> receiver = new Receiver<double>();
+
+            DataSubscription<double> sub = new DataSubscription<double>(receiver, "double");
+
+            stream.AddSubscriber(sub);
+            stream.Parse(JSON);
+            stream.DeliverSubscriptions();
+
+            Assert.AreEqual(2.0, receiver.receivedPayload);
         }
 
         [TestMethod]
-        public void TestMethod1()
+        public void ReceiveString()
         {
-            DataSubscription<string> sub = new DataSubscription<string>(this, "test");
+            DataStream stream = new DataStream();
+            Receiver<string> receiver = new Receiver<string>();
 
+            DataSubscription<string> sub = new DataSubscription<string>(receiver, "string");
 
-            Dictionary<string, string> json_parsed = Newtonsoft.Json.JsonConvert.DeserializeObject<Dictionary<string, string>>(JSON);
+            stream.AddSubscriber(sub);
+            stream.Parse(JSON);
+            stream.DeliverSubscriptions();
 
-            received_payload = (string) sub.receive(json_parsed);
-            Assert.AreEqual(received_payload, value);
+            Assert.AreEqual("this is a string", receiver.receivedPayload);
         }
 
+        [TestMethod]
+        public void ReceiveFloat()
+        {
+            DataStream stream = new DataStream();
+            Receiver<float> receiver = new Receiver<float>();
 
+            DataSubscription<float> sub = new DataSubscription<float>(receiver, "float");
+
+            stream.AddSubscriber(sub);
+            stream.Parse(JSON);
+            stream.DeliverSubscriptions();
+
+            Assert.AreEqual(3.2f, receiver.receivedPayload);
+        }
+
+        [TestMethod]
+        public void ReceiveInt()
+        {
+            DataStream stream = new DataStream();
+            Receiver<int> receiver = new Receiver<int>();
+
+            DataSubscription<int> sub = new DataSubscription<int>(receiver, "int");
+
+            stream.AddSubscriber(sub);
+            stream.Parse(JSON);
+            stream.DeliverSubscriptions();
+
+            Assert.AreEqual(5, receiver.receivedPayload);
+        }
+
+        [TestMethod]
+        public void ReceiveLong()
+        {
+            DataStream stream = new DataStream();
+            Receiver<long> receiver = new Receiver<long>();
+
+            DataSubscription<long> sub = new DataSubscription<long>(receiver, "long");
+
+            stream.AddSubscriber(sub);
+            stream.Parse(JSON);
+            stream.DeliverSubscriptions();
+
+            Assert.AreEqual(5L, receiver.receivedPayload);
+        }
+
+        [TestMethod]
+        public void ReceiveGenericList()
+        {
+            DataStream stream = new DataStream();
+            Receiver<Newtonsoft.Json.Linq.JArray> receiver = new Receiver<Newtonsoft.Json.Linq.JArray>();
+
+            DataSubscription<Newtonsoft.Json.Linq.JArray> sub = new DataSubscription<Newtonsoft.Json.Linq.JArray>(receiver, "genericlist");
+
+            stream.AddSubscriber(sub);
+            stream.Parse(JSON);
+            stream.DeliverSubscriptions();
+
+            Newtonsoft.Json.Linq.JArray actual = new Newtonsoft.Json.Linq.JArray { "one", "two", "three" };
+
+            Assert.AreEqual(actual, receiver.receivedPayload);
+        }
+
+    }
+
+    public class Receiver<T> : DataReceiver<T>
+    {
+        public T receivedPayload;
+        
+        public void ReceivePayload(T payload)
+        {
+            receivedPayload = payload;
+            Console.WriteLine(payload);
+        }
     }
 }
